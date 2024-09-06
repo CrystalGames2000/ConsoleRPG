d3c2e82f2b6b1db00ea8b21f168595ca87bf8550
CrystalGames2000 <ofirabe09@gmail.com>
2024-09-06 14:05:31 +0300

    Completed Basic Weapon Interface

--- ConsoleRPG/Mechanics/Items/Weapon.cs
+++ ConsoleRPG/Mechanics/Items/Weapon.cs
@@ -1,4 +1,13 @@
 ﻿public class Weapon: Item
 {
     string Item.Name { get; set; }
+
+    int? Cost { get; set; }
+    string? DmgS {  get; set; }
+    string? DmgM { get; set; }
+    int? Critical {  get; set; }
+    int? Range { get; set; }
+    int? Weight { get; set; }
+    string? Type { get; set; }
+    string[]? Special {  get; set; }
 }
